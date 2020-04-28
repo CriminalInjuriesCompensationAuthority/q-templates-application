c4a361b14ec31dff1b57a33a59cbb1f4b755dfc0
BarryPiccinni <Barry.Piccinni@cica.gov.uk>
2020-04-28 10:29:23 +0100

    Release candidate (#21)
    * Add template v0.1 for assessment.
    
    * renameFile
    
    * Reset to base template
    
    * Add new flow part 2
    
    * Add new flow part 4 (#8)
    
    * Add new flow part 2
    
    * Add new flow part 4
    
    * Update template
    
    * Add new flow part 4
    
    * Update template
    
    * Add new flow part 1 (#5)
    
    * Add new flow part 2
    
    * Add new flow part 1
    
    * Content changes requested in review
    
    * Add new flow part 4 (#8)
    
    * Add new flow part 2
    
    * Add new flow part 4
    
    * Update template
    
    * Add new flow part 4
    
    * Update template
    
    * Content changes requested by PR
    
    * Add new flow part 1
    
    * Content changes requested in review
    
    * Content changes requested by PR
    
    * Add new flow part 3 (#7)
    
    * Various content fixes (#9)
    
    * Various content fixes
    
    * Remove 'rediect to our other application' page
    
    * Update base template to pass validation
    
    * Tests (#12)
    
    * Add template tests
    
    * Update q-template-validator to v0.0.2
    
    Co-authored-by: sinclairs <stephen.sinclair@cica.gov.uk>
    
    * Move template to index.js
    
    * Bump q-template-validator version (#14)
    
    * Add govuk-link class to all hyperlinks (#16)
    
    * Rename module to q-templates-application (#15)
    
    * Content fixes (#18)
    
    * Initial content fixes
    
    * Fixed order of police questions
    
    * Add various minor content changes
    
    * SMS (#17)
    
    * Add SMS
    
    * Revert changes
    
    * Add AJV custom format for UK mobiles
    
    * Add govuk-link class
    
    * Update invalidExamples
    
    * Update version
    
    * Add SMS to confirmation page (#19)
    
    Co-authored-by: sinclairs <stephen.sinclair@cica.gov.uk>
    Co-authored-by: Adrian Roworth <adrian.roworth@CICA.GOV.UK>

---
+++ index.test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const createTemplateValidator = require('q-template-validator');
+const ajvFormatsMobileUk = require('ajv-formats-mobile-uk');
+const template = require('./index');
+
+describe('application template', () => {
+    it('should be valid', () => {
+        const qTemplateValidator = createTemplateValidator({
+            questionnaireTemplate: template,
+            customSchemaFormats: {
+                'mobile-uk': ajvFormatsMobileUk
+            }
+        });
+        const valid = qTemplateValidator.validateTemplate();
+
+        expect(valid).toEqual(true);
+    });
+});
