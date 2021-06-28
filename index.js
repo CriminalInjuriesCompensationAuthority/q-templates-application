e24751e320e3fe3c23cd60634c101ca1e0e437d1
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2021-06-28 10:24:45 +0100

    Add contextualised infections to non-Sexual-assault cases

--- index.js
+++ index.js
@@ -9580,6 +9580,35 @@ module.exports = {
             }
         },
         'p-applicant-non-sa-infections': {
+            l10n: {
+                vars: {
+                    lng: 'en',
+                    context: {
+                        $data:
+                            '/answers/p-applicant-who-are-you-applying-for/q-applicant-who-are-you-applying-for'
+                    },
+                    ns: 'p-applicant-non-sa-infections'
+                },
+                translations: [
+                    {
+                        language: 'en',
+                        namespace: 'p-applicant-non-sa-infections',
+                        resources: {
+                            'q-applicant-non-sa-infections': {
+                                title: 'Do you have HIV or hepatitis as a result of the crime?',
+                                'title_someone-else':
+                                    'Do they have HIV or hepatitis as a result of the crime?',
+                                error: {
+                                    required:
+                                        'Select yes if you have HIV or hepatitis as a result of the crime',
+                                    'required_someone-else':
+                                        'Select yes if they have HIV or hepatitis as a result of the crime'
+                                }
+                            }
+                        }
+                    }
+                ]
+            },
             schema: {
                 $schema: 'http://json-schema.org/draft-07/schema#',
                 type: 'object',
@@ -9588,13 +9617,13 @@ module.exports = {
                 properties: {
                     'q-applicant-non-sa-infections': {
                         type: 'boolean',
-                        title: 'Do you have HIV or hepatitis as a result of the crime?'
+                        title: 'l10nt:q-applicant-non-sa-infections.title{?lng,context,ns}'
                     }
                 },
                 errorMessage: {
                     required: {
                         'q-applicant-non-sa-infections':
-                            'Select yes if you have HIV or hepatitis as a result of the crime'
+                            'l10nt:q-applicant-non-sa-infections.error.required{?lng,context,ns}'
                     }
                 },
                 examples: [
