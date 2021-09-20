278384665ad8079e443db78f972650420317db62
Stephen S <Stephen.Sinclair@CICA.GOV.UK>
2021-09-20 16:31:15 +0100

    Update 'build.js' to handle missing 'dist' directory

--- src/build.js
+++ src/build.js
@@ -8,8 +8,14 @@ const deleteKeys = require('./lib/deleteKeys');
 // deleteKeys(template, process.argv[2]);
 const trimmedTemplate = deleteKeys(template, ['examples', 'invalidExamples']);
 
-fs.writeFile('./dist/template.json', JSON.stringify(trimmedTemplate), err => {
-    if (err) {
-        throw err;
+fs.mkdir('./dist', {recursive: true}, makeDirErr => {
+    if (makeDirErr) {
+        throw makeDirErr;
     }
+
+    fs.writeFile('./dist/template.json', JSON.stringify(trimmedTemplate), {}, writeFileErr => {
+        if (writeFileErr) {
+            throw writeFileErr;
+        }
+    });
 });
