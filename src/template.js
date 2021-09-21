75c15a535acaf00726483c3e2e30a38b58a3e30c
Stephen S <Stephen.Sinclair@CICA.GOV.UK>
2021-09-21 11:19:14 +0100

    Update 'p-applicant-enter-your-address' schema to use allOf

--- src/template.js
+++ src/template.js
@@ -2086,58 +2086,92 @@ module.exports = {
             schema: {
                 $schema: 'http://json-schema.org/draft-07/schema#',
                 type: 'object',
-                title: 'Enter your address',
-                required: ['q-applicant-building-and-street', 'q-applicant-town-or-city'],
-                additionalProperties: false,
-                properties: {
-                    'q-applicant-building-and-street': {
-                        type: 'string',
-                        title: 'Building and street',
-                        maxLength: 60,
+                allOf: [
+                    {
+                        title: 'Enter your address',
+                        required: ['q-applicant-building-and-street', 'q-applicant-town-or-city'],
+                        propertyNames: {
+                            enum: [
+                                'q-applicant-building-and-street',
+                                'q-applicant-building-and-street-2',
+                                'q-applicant-town-or-city',
+                                'q-applicant-county',
+                                'q-applicant-postcode'
+                            ]
+                        },
+                        allOf: [
+                            {
+                                properties: {
+                                    'q-applicant-building-and-street': {
+                                        type: 'string',
+                                        title: 'Building and street',
+                                        maxLength: 60,
+                                        errorMessage: {
+                                            maxLength:
+                                                'First line of address must be less than 60 characters'
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                properties: {
+                                    'q-applicant-building-and-street-2': {
+                                        type: 'string',
+                                        title: 'Building and street line 2',
+                                        maxLength: 60,
+                                        errorMessage: {
+                                            maxLength:
+                                                'Second line of address must be less than 60 characters'
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                properties: {
+                                    'q-applicant-town-or-city': {
+                                        type: 'string',
+                                        title: 'Town or city',
+                                        maxLength: 32,
+                                        errorMessage: {
+                                            maxLength: 'Town or city must be 32 characters or less'
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                properties: {
+                                    'q-applicant-county': {
+                                        type: 'string',
+                                        title: 'County (optional)',
+                                        maxLength: 32,
+                                        errorMessage: {
+                                            maxLength: 'County must be 32 characters or less'
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                properties: {
+                                    'q-applicant-postcode': {
+                                        type: 'string',
+                                        title: 'Postcode (optional)',
+                                        maxLength: 10,
+                                        errorMessage: {
+                                            maxLength: 'Postcode must be 10 characters or less'
+                                        }
+                                    }
+                                }
+                            }
+                        ],
                         errorMessage: {
-                            maxLength: 'First line of address must be less than 60 characters'
-                        }
-                    },
-                    'q-applicant-building-and-street-2': {
-                        type: 'string',
-                        title: 'Building and street line 2',
-                        maxLength: 60,
-                        errorMessage: {
-                            maxLength: 'Second line of address must be less than 60 characters'
-                        }
-                    },
-                    'q-applicant-town-or-city': {
-                        type: 'string',
-                        title: 'Town or city',
-                        maxLength: 32,
-                        errorMessage: {
-                            maxLength: 'Town or city must be 32 characters or less'
-                        }
-                    },
-                    'q-applicant-county': {
-                        type: 'string',
-                        title: 'County (optional)',
-                        maxLength: 32,
-                        errorMessage: {
-                            maxLength: 'County must be 32 characters or less'
-                        }
-                    },
-                    'q-applicant-postcode': {
-                        type: 'string',
-                        title: 'Postcode (optional)',
-                        maxLength: 10,
-                        errorMessage: {
-                            maxLength: 'Postcode must be 10 characters or less'
+                            required: {
+                                'q-applicant-building-and-street':
+                                    'Enter the building and street where you live',
+                                'q-applicant-town-or-city': 'Enter the town or city where you live'
+                            }
                         }
                     }
-                },
-                errorMessage: {
-                    required: {
-                        'q-applicant-building-and-street':
-                            'Enter the building and street where you live',
-                        'q-applicant-town-or-city': 'Enter the town or city where you live'
-                    }
-                },
+                ],
                 examples: [
                     {
                         'q-applicant-building-and-street': '1 Foo Lane',
