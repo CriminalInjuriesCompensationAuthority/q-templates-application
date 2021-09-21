98b3f8de0c72fb21eb8eab556db3482d36679df8
Stephen S <Stephen.Sinclair@CICA.GOV.UK>
2021-09-21 11:41:13 +0100

    Update 'p-gp-enter-your-address' schema to use allOf

--- src/template.js
+++ src/template.js
@@ -3804,62 +3804,98 @@ module.exports = {
             schema: {
                 $schema: 'http://json-schema.org/draft-07/schema#',
                 type: 'object',
-                title: "What is the GP's address?",
-                required: [
-                    'q-gp-building-and-street',
-                    'q-gp-town-or-city',
-                    'q-gp-building-and-street2'
+                allOf: [
+                    {
+                        title: "What is the GP's address?",
+                        required: [
+                            'q-gp-building-and-street',
+                            'q-gp-town-or-city',
+                            'q-gp-building-and-street2'
+                        ],
+                        propertyNames: {
+                            enum: [
+                                'q-gp-building-and-street',
+                                'q-gp-building-and-street2',
+                                'q-gp-town-or-city',
+                                'q-gp-county',
+                                'q-gp-postcode'
+                            ]
+                        },
+                        allOf: [
+                            {
+                                properties: {
+                                    'q-gp-building-and-street': {
+                                        type: 'string',
+                                        title: 'Practice name',
+                                        maxLength: 60,
+                                        errorMessage: {
+                                            maxLength:
+                                                'Practice name must be less than 60 characters'
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                properties: {
+                                    'q-gp-building-and-street2': {
+                                        type: 'string',
+                                        title: 'Building and street',
+                                        maxLength: 60,
+                                        errorMessage: {
+                                            maxLength:
+                                                'Building and street must be less than 60 characters'
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                properties: {
+                                    'q-gp-town-or-city': {
+                                        type: 'string',
+                                        title: 'Town or city',
+                                        maxLength: 32,
+                                        errorMessage: {
+                                            maxLength: 'Town or city must be 32 characters or less'
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                properties: {
+                                    'q-gp-county': {
+                                        type: 'string',
+                                        title: 'County (optional)',
+                                        maxLength: 32,
+                                        errorMessage: {
+                                            maxLength: 'County must be 32 characters or less'
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                properties: {
+                                    'q-gp-postcode': {
+                                        type: 'string',
+                                        title: 'Postcode (optional)',
+                                        maxLength: 10,
+                                        errorMessage: {
+                                            maxLength: 'Postcode must be 10 characters or less'
+                                        }
+                                    }
+                                }
+                            }
+                        ],
+                        errorMessage: {
+                            required: {
+                                'q-gp-building-and-street': "Enter the name of your GP's practice",
+                                'q-gp-building-and-street2':
+                                    'Enter the building and street of your GP',
+                                'q-gp-town-or-city':
+                                    "Enter the town or city where your GP's practice is"
+                            }
+                        }
+                    }
                 ],
-                additionalProperties: false,
-                properties: {
-                    'q-gp-building-and-street': {
-                        type: 'string',
-                        title: 'Practice name',
-                        maxLength: 60,
-                        errorMessage: {
-                            maxLength: 'Practice name must be less than 60 characters'
-                        }
-                    },
-                    'q-gp-building-and-street2': {
-                        type: 'string',
-                        title: 'Building and street',
-                        maxLength: 60,
-                        errorMessage: {
-                            maxLength: 'Building and street must be less than 60 characters'
-                        }
-                    },
-                    'q-gp-town-or-city': {
-                        type: 'string',
-                        title: 'Town or city',
-                        maxLength: 32,
-                        errorMessage: {
-                            maxLength: 'Town or city must be 32 characters or less'
-                        }
-                    },
-                    'q-gp-county': {
-                        type: 'string',
-                        title: 'County (optional)',
-                        maxLength: 32,
-                        errorMessage: {
-                            maxLength: 'County must be 32 characters or less'
-                        }
-                    },
-                    'q-gp-postcode': {
-                        type: 'string',
-                        title: 'Postcode (optional)',
-                        maxLength: 10,
-                        errorMessage: {
-                            maxLength: 'Postcode must be 10 characters or less'
-                        }
-                    }
-                },
-                errorMessage: {
-                    required: {
-                        'q-gp-building-and-street': "Enter the name of your GP's practice",
-                        'q-gp-building-and-street2': 'Enter the building and street of your GP',
-                        'q-gp-town-or-city': "Enter the town or city where your GP's practice is"
-                    }
-                },
                 examples: [
                     {
                         'q-gp-building-and-street': '1 Foo Lane',
