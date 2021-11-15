52de88ae18dedcbe54f224d017113daeb0ff12ad
neil-stephen-mcgonigle <neil.mcgonigle@cica.gov.uk>
2021-11-15 17:05:36 +0000

    Merge branch 'l10n-rc' of https://github.com/CriminalInjuriesCompensationAuthority/q-templates-application into l10n-rc

--- src/lib/resource/sections/applicant-select-non-sa-infections.js
+++ src/lib/resource/sections/applicant-select-non-sa-infections.js
@@ -96,19 +96,23 @@ module.exports = {
             ]
         }
     },
-    route: {on: {ANSWER: [
-        {
-            target: 'p--context-pregnancy',
-            cond: [
-                'operatorDateCompareToToday',
-                '>',
-                '$.answers.p-applicant-enter-your-date-of-birth.q-applicant-enter-your-date-of-birth',
-                7,
-                'years'
+    route: {
+        on: {
+            ANSWER: [
+                {
+                    target: 'p--context-pregnancy',
+                    cond: [
+                        'operatorDateCompareToToday',
+                        '>',
+                        '$.answers.p-applicant-enter-your-date-of-birth.q-applicant-enter-your-date-of-birth',
+                        7,
+                        'years'
+                    ]
+                },
+                {
+                    target: 'p--context-dmi-details'
+                }
             ]
-        },
-        {
-            target: 'p--context-dmi-details'
         }
-    ]}}
+    }
 };
