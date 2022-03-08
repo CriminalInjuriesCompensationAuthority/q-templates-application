76f6ca04c68bae3b5b1395c30283aef7c2adc224
neil-stephen-mcgonigle <Neil.Mcgonigle@CICA.GOV.UK>
2022-03-08 08:49:43 +0000

    Update template for q-schema v4.0.0

--- src/template.js
+++ src/template.js
@@ -581,7 +581,7 @@ module.exports = {
                 main_applicant_details: {
                     title: 'Your details'
                 },
-                'default': {
+                default: {
                     title: 'Other Information'
                 }
             }
