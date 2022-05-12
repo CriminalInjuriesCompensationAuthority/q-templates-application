e9524a012586dd8e4c9338dd8345dae3a01c7555
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2022-05-12 12:25:03 +0100

    Update impact of injury sections

--- src/lib/resource/sections/applicant-future-work.js
+++ src/lib/resource/sections/applicant-future-work.js
@@ -10,8 +10,7 @@ module.exports = {
             properties: {
                 'q-applicant-future-work': {
                     type: 'string',
-                    title:
-                        "Has the victim's ability to work in future been affected by their injuries?",
+                    title: 'Has their ability to work in future been affected by their injuries?',
                     oneOf: [
                         {
                             title: 'Yes',
@@ -43,7 +42,7 @@ module.exports = {
             errorMessage: {
                 required: {
                     'q-applicant-future-work':
-                        "Select yes if the victim's ability to work in future has been affected by their injuries"
+                        'Select yes if their ability to work in future has been affected by their injuries'
                 }
             },
             examples: [
