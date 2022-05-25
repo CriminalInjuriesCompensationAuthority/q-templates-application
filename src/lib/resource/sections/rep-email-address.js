be6894f988b73a7ffb8133aed9ef96c4497ab9ef
neil-stephen-mcgonigle <Neil.Mcgonigle@CICA.GOV.UK>
2022-05-25 16:13:49 +0100

    Add rep additional contact details email address section and route

---
+++ src/lib/resource/sections/rep-email-address.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+    section: {
+        schema: {
+            $schema: 'http://json-schema.org/draft-07/schema#',
+            type: 'object',
+            additionalProperties: false,
+            properties: {
+                'q-rep-email-address': {
+                    type: 'string',
+                    title: 'Enter your email address',
+                    description:
+                        'We may use this to contact you if we need to clarify something in this application (optional).',
+                    maxLength: 50,
+                    format: 'email',
+                    errorMessage: {
+                        maxLength: 'Email address must be 50 characters or less',
+                        format:
+                            'Enter an email address in the correct format, like name@example.com'
+                    },
+                    meta: {
+                        classifications: {
+                            theme: 'reps-details'
+                        },
+                        summary: {
+                            title: 'Email address'
+                        }
+                    }
+                }
+            },
+            examples: [
+                {
+                    'q-rep-email-address': 'foo@hhjhjk34h5jkh24kj5h2k45.com'
+                },
+                {}
+            ],
+            invalidExamples: [
+                {
+                    'q-rep-email-address': 12345
+                }
+            ]
+        }
+    },
+    route: {
+        on: {
+            ANSWER: [
+                {
+                    target: 'p--before-you-continue'
+                }
+            ]
+        }
+    }
+};
