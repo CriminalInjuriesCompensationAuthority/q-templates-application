a540478d0b69654c687a6c8ea101cf8ede78b5db
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2022-06-01 15:45:45 +0100

    Cater for Node + NPM bumps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
-/*! m0-start */
+'use strict';
+
 const config = {
     extends: ['airbnb-base', 'prettier', 'plugin:jest/recommended'],
     env: {
@@ -6,7 +7,7 @@ const config = {
     },
     // overwrite airbnb-base to use commonjs instead of ES6 modules
     parserOptions: {
-        ecmaVersion: 9,
+        ecmaVersion: 2021,
         sourceType: 'script',
         ecmaFeatures: {
             modules: false
@@ -15,16 +16,13 @@ const config = {
     rules: {
         'prettier/prettier': ['error'],
         'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
-        'curly': ['error', 'all'],
+        curly: ['error', 'all'],
         'jest/expect-expect': ['error'],
         // https://github.com/eslint/eslint/issues/8953#issuecomment-317697474
-        strict: ['error', 'safe']
+        strict: ['error', 'safe'],
+        'import/extensions': ['off']
     },
     plugins: ['prettier']
 };
-/*! m0-end */
 
-/*! m0-start */
 module.exports = config;
-/*! m0-end */
-
