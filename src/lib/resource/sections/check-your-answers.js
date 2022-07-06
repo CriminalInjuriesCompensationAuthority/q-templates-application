cfc3df7a75d8e451889d2ea23352668916df28f2
Barry Piccinni <barry.piccinni@cica.gov.uk>
2022-07-06 18:43:35 +0100

    Add routing for download page

--- src/lib/resource/sections/check-your-answers.js
+++ src/lib/resource/sections/check-your-answers.js
@@ -866,7 +866,60 @@ module.exports = {
         on: {
             ANSWER: [
                 {
-                    target: 'p--download-your-answers'
+                    target: 'p-applicant-declaration',
+                    cond: [
+                        '==',
+                        '$.answers.p-applicant-who-are-you-applying-for.q-applicant-who-are-you-applying-for',
+                        'myself'
+                    ]
+                },
+                {
+                    target: 'p--download-your-answers',
+                    cond: [
+                        'or',
+                        [
+                            '==',
+                            '$.answers.p-rep-type.q-rep-type',
+                            'SOLS'
+                        ],
+                        [
+                            '==',
+                            '$.answers.p-rep-type.q-rep-type',
+                            'CMCO'
+                        ],
+                        [
+                            '==',
+                            '$.answers.p-rep-type.q-rep-type',
+                            'SUPP'
+                        ],
+                        [
+                            '==',
+                            '$.answers.p-rep-type.q-rep-type',
+                            'SSER'
+                        ]
+                    ]
+                },
+                {
+                    target: 'p-mainapplicant-declaration-under-12',
+                    cond: [
+                        'and',
+                        [
+                            '==',
+                            '$.answers.p-applicant-who-are-you-applying-for.q-applicant-who-are-you-applying-for',
+                            'someone-else'
+                        ],
+                        [
+                            'dateCompare',
+                            '$.answers.p-applicant-enter-your-date-of-birth.q-applicant-enter-your-date-of-birth', // this date ...
+                            '<', // is less than ...
+                            '-12', // 12 ...
+                            'years' // years (before, due to the negative (-12) ...
+                            // today's date (no second date given. defaults to today's date).
+                        ]
+                    ]
+                },
+                {
+                    target: 'p-mainapplicant-declaration-12-and-over'
                 }
             ]
         }
