87504df25be417eb4d207b3b9e2c520ebadd94c9
Stephen S <Stephen.Sinclair@CICA.GOV.UK>
2022-07-28 15:23:52 +0100

    Add 'p--represents-legal-authority' section

---
+++ src/lib/resource/sections/flow-represents-legal-authority.js
@@ -0,0 +1,64 @@
+'use strict';
+
+module.exports = {
+    section: {
+        schema: {
+            $schema: 'http://json-schema.org/draft-07/schema#',
+            type: 'object',
+            required: ['q--represents-legal-authority'],
+            additionalProperties: false,
+            properties: {
+                'q--represents-legal-authority': {
+                    type: 'boolean',
+                    title: 'Does someone else have legal authority to act on behalf of them?',
+                    oneOf: [
+                        {
+                            title: 'Yes',
+                            const: true
+                        },
+                        {
+                            title: 'No',
+                            const: false
+                        }
+                    ],
+                    meta: {
+                        classifications: {
+                            theme: 'main-applicant-details'
+                        },
+                        summary: {
+                            title: 'Does someone else have legal authority?'
+                        }
+                    }
+                }
+            },
+            errorMessage: {
+                required: {
+                    'q--represents-legal-authority':
+                        'Select yes if someone else has legal authority to act on behalf of them'
+                }
+            },
+            examples: [
+                {
+                    'q--represents-legal-authority': true
+                },
+                {
+                    'q--represents-legal-authority': false
+                }
+            ],
+            invalidExamples: [
+                {
+                    'q--represents-legal-authority': 'foo'
+                }
+            ]
+        }
+    },
+    route: {
+        on: {
+            ANSWER: [
+                {
+                    target: 'p--context-authority'
+                }
+            ]
+        }
+    }
+};
