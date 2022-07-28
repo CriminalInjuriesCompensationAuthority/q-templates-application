9e1470adba8688220fe11d98c4b5e07dce3f4edf
Stephen S <Stephen.Sinclair@CICA.GOV.UK>
2022-07-28 15:22:28 +0100

    Add 'p--has-legal-authority' section

---
+++ src/lib/resource/sections/flow-has-legal-authority.js
@@ -0,0 +1,126 @@
+'use strict';
+
+module.exports = {
+    section: {
+        l10n: {
+            vars: {
+                lng: 'en',
+                ns: 'p--has-legal-authority'
+            },
+            translations: [
+                {
+                    language: 'en',
+                    namespace: 'p--has-legal-authority',
+                    resources: {
+                        'q--has-legal-authority': {
+                            title: {
+                                proxy: {
+                                    child: 'Do you have parental responsibility for them?',
+                                    adult:
+                                        'Do you have legal authority to act on behalf of the victim?'
+                                }
+                            },
+                            error: {
+                                required: {
+                                    proxy: {
+                                        child:
+                                            'Select yes if you have parental responsibility for them',
+                                        adult:
+                                            'Select yes if you have legal authority to act on behalf of the victim'
+                                    }
+                                }
+                            },
+                            meta: {
+                                summary: {
+                                    title: {
+                                        proxy: {
+                                            child: 'Do you have parental responsibility for them?',
+                                            adult: 'Do you have legal authority?'
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            ]
+        },
+        schema: {
+            $schema: 'http://json-schema.org/draft-07/schema#',
+            type: 'object',
+            required: ['q--has-legal-authority'],
+            additionalProperties: false,
+            properties: {
+                'q--has-legal-authority': {
+                    type: 'boolean',
+                    // prettier-ignore
+                    title: ['|l10nt',
+                        ['|role.all', 'proxy', 'child'], 'q--has-legal-authority.title.proxy.child',
+                        ['|role.all', 'proxy', 'adult'], 'q--has-legal-authority.title.proxy.adult'
+                    ],
+                    oneOf: [
+                        {
+                            title: 'Yes',
+                            const: true
+                        },
+                        {
+                            title: 'No',
+                            const: false
+                        }
+                    ],
+                    meta: {
+                        classifications: {
+                            theme: 'main-applicant-details'
+                        },
+                        summary: {
+                            // prettier-ignore
+                            title: ['|l10nt',
+                                ['|role.all', 'proxy', 'child'], 'q--has-legal-authority.meta.summary.title.proxy.child',
+                                ['|role.all', 'proxy', 'adult'], 'q--has-legal-authority.meta.summary.title.proxy.adult'
+                            ]
+                        }
+                    }
+                }
+            },
+            errorMessage: {
+                required: {
+                    // prettier-ignore
+                    'q--has-legal-authority': ['|l10nt',
+                        ['|role.all', 'proxy', 'child'], 'q--has-legal-authority.error.required.proxy.child',
+                        ['|role.all', 'proxy', 'adult'], 'q--has-legal-authority.error.required.proxy.adult'
+                    ]
+                }
+            },
+            examples: [
+                {
+                    'q--has-legal-authority': true
+                },
+                {
+                    'q--has-legal-authority': false
+                }
+            ],
+            invalidExamples: [
+                {
+                    'q--has-legal-authority': 'foo'
+                }
+            ]
+        }
+    },
+    route: {
+        on: {
+            ANSWER: [
+                {
+                    target: 'p--represents-legal-authority',
+                    // prettier-ignore
+                    cond: ['and',
+                        ['==', '$.answers.p--has-legal-authority.q--has-legal-authority', false],
+                        ['==', '$.answers.p-applicant-are-you-18-or-over.q-applicant-are-you-18-or-over', true]
+                    ]
+                },
+                {
+                    target: 'p--context-authority'
+                }
+            ]
+        }
+    }
+};
