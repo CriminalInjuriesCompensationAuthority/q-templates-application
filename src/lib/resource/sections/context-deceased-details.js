87329ffc84f0416a11c39258db25f2bfd15518d2
Stephen McNeill <Stephen.McNeill@CICA.GOV.UK>
2023-01-19 09:56:04 +0000

    Added new file and changes for context-deceased-details page

---
+++ src/lib/resource/sections/context-deceased-details.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+    section: {
+        schema: {
+            $schema: 'http://json-schema.org/draft-07/schema#',
+            type: 'object',
+            additionalProperties: false,
+            properties: {
+                transition: {
+                    title: 'About the person who died',
+                    description:
+                        '<p class="govuk-body">We\'re now going to ask you some questions about the person who died. This includes their<br><ul><li>name</li><li>date of birth</li><li>date of death</li><li>address</li></ul></p>{{ govukButton({text: "Continue",href: "https://www.cica.gov.uk/OAS/Account/create",isStartButton: true}) }}'
+                }
+            },
+            examples: [{}],
+            invalidExamples: [
+                {
+                    foo: 'bar'
+                }
+            ]
+        }
+    },
+    route: {
+        type: 'final'
+    }
+};
