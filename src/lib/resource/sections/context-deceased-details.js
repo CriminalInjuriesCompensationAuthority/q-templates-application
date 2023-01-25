877bd28917f562f6a03c648779d9471d923063e7
Stephen McNeill <Stephen.McNeill@CICA.GOV.UK>
2023-01-25 17:00:04 +0000

    Added routing and removed inline routing code

--- src/lib/resource/sections/context-deceased-details.js
+++ src/lib/resource/sections/context-deceased-details.js
@@ -10,18 +10,20 @@ module.exports = {
                 transition: {
                     title: 'About the person who died',
                     description:
-                        '<p class="govuk-body">We\'re now going to ask you some questions about the person who died. This includes their<br><ul class="govuk-list govuk-list--bullet"><li>name</li><li>date of birth</li><li>date of death</li><li>address</li></ul></p>{{ govukButton({text: "Continue",href: "https://www.cica.gov.uk/OAS/Account/create",isStartButton: true}) }}'
+                        '<p class="govuk-body">We\'re now going to ask you some questions about the person who died. This includes their<br><ul class="govuk-list govuk-list--bullet"><li>name</li><li>date of birth</li><li>date of death</li><li>address</li></ul></p>'
                 }
             },
             examples: [{}],
-            invalidExamples: [
-                {
-                    foo: 'bar'
-                }
-            ]
+            invalidExamples: [{}]
         }
     },
     route: {
-        type: 'final'
+        on: {
+            ANSWER: [
+                {
+                    target: 'p-deceased-name'
+                }
+            ]
+        }
     }
 };
