fb64b05ffe20d0eda938b4180b008af986b620c3
Stephen McNeill <Stephen.McNeill@CICA.GOV.UK>
2023-02-17 10:31:13 +0000

    Re-added user role replacement routing for DOB compare conditions

--- src/lib/resource/sections/flow-has-legal-authority.js
+++ src/lib/resource/sections/flow-has-legal-authority.js
@@ -114,15 +114,8 @@ module.exports = {
                     // prettier-ignore
                     cond: [
                         'and',
-                        ['==', '$.answers.p--has-legal-authority.q--has-legal-authority', false],
-                        [
-                        'dateCompare',
-                        '$.answers.p-applicant-enter-your-date-of-birth.q-applicant-enter-your-date-of-birth', // this date ...
-                        '<', // is greater than or equal to ...
-                        '-18', // 18 ...
-                        'years' // years (before, due to the negative (-18) ...
-                        // today's date (no second date given. defaults to today's date).
-                        ]                                              
+                        ['|role.all', 'child'],
+                        ['==', '$.answers.p--has-legal-authority.q--has-legal-authority', false],                                              
                     ]
                 },
                 {
--- src/lib/resource/sections/mainapplicant-relationship.js
+++ src/lib/resource/sections/mainapplicant-relationship.js
@@ -120,31 +120,13 @@ module.exports = {
             ANSWER: [
                 {
                     target: 'p--before-you-continue',
-                    cond: [
-                        'and',
-                        [
-                            'dateCompare',
-                            '$.answers.p-applicant-enter-your-date-of-birth.q-applicant-enter-your-date-of-birth', // this date ...
-                            '>=', // is greater than or equal to ...
-                            '-18', // 18 ...
-                            'years' // years (before, due to the negative (-18) ...
-                            // today's date (no second date given. defaults to today's date).
-                        ],
-                        ['|role.all', 'mainapplicant']
-                    ]
+                    cond: ['|role.all', 'mainapplicant', 'adult']
                 },
                 {
                     target: 'p--context-rep-details',
                     cond: [
                         'and',
-                        [
-                            'dateCompare',
-                            '$.answers.p-applicant-enter-your-date-of-birth.q-applicant-enter-your-date-of-birth', // this date ...
-                            '>=', // is greater than or equal to ...
-                            '-18', // 18 ...
-                            'years' // years (before, due to the negative (-18) ...
-                            // today's date (no second date given. defaults to today's date).
-                        ],
+                        ['|role.all', 'adult'],
                         // Rep role
                         ['==', '$.answers.p-mainapplicant-parent.q-mainapplicant-parent', false],
                         ['==', '$.answers.p--has-legal-authority.q--has-legal-authority', false]
