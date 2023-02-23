db43716e72e2f028e8ba3b4db2f5075a6093c8dc
stephenjmcneill1971 <stephen.mcneill@cica.gov.uk>
2023-02-23 11:44:05 +0000

    Added new page applicant-funeral-costs-paid

---
+++ src/lib/resource/sections/applicant-funeral-costs-paid.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = {
+    section: {
+        schema: {
+            $schema: 'http://json-schema.org/draft-07/schema#',
+            type: 'object',
+            required: ['q-applicant-funeral-costs-paid'],
+            additionalProperties: false,
+            properties: {
+                'q-applicant-funeral-costs-paid': {
+                    type: 'boolean',
+                    title: 'Did you pay for any of the funeral costs?',
+                    oneOf: [
+                        {
+                            title: 'Yes',
+                            const: true
+                        },
+                        {
+                            title: 'No',
+                            const: false
+                        }
+                    ],
+                    meta: {
+                        classifications: {
+                            theme: 'funeral_costs'
+                        }
+                    }
+                }
+            },
+            errorMessage: {
+                required: {
+                    'q-applicant-funeral-costs-paid':
+                        'Select yes if you paid for any of the funeral costs'
+                }
+            },
+            examples: [
+                {
+                    'q-applicant-funeral-costs-paid': true
+                },
+                {
+                    'q-applicant-funeral-costs-paid': false
+                }
+            ],
+            invalidExamples: [
+                {
+                    'q-applicant-funeral-costs-paid': 'foo'
+                }
+            ]
+        }
+    },
+    route: {
+        on: {
+            ANSWER: [
+                {
+                    target: 'p-applicant-funeral-costs-other-contributor'
+                }
+            ]
+        }
+    }
+};
