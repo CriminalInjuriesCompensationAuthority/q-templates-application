5ebd083ca5667150a4ea6c07d53ec1a11c454030
stephenjmcneill1971 <stephen.mcneill@cica.gov.uk>
2023-03-13 15:49:39 +0000

    Corrected meta data error and page format and changes summary title effecting check all answers page

--- src/lib/resource/sections/applicant-funeral-costs-who-contributed.js
+++ src/lib/resource/sections/applicant-funeral-costs-who-contributed.js
@@ -5,45 +5,53 @@ module.exports = {
         schema: {
             $schema: 'http://json-schema.org/draft-07/schema#',
             type: 'object',
-            required: ['q-applicant-funeral-costs-who-contributed'],
             allOf: [
                 {
-                    properties: {
-                        'q-applicant-funeral-costs-who-contributed': {
-                            type: 'string',
-                            title: 'Who else has contributed to the funeral costs?',
-                            description:
-                                'Enter the names of people, charities or organisations who helped to pay for the funeral.',
-                            maxLength: 2000,
-                            errorMessage: {
-                                maxLength: 'Description must be 2000 characters or less'
-                            },
-                            meta: {
-                                classifications: {
-                                    theme: 'funeral-costs'
-                                },
-                                summary: {
-                                    title: 'Funeral costs'
+                    title: 'Who else has contributed to the funeral costs?',
+                    meta: {
+                        compositeId: 'funeral-costs-id',
+                        classifications: {
+                            theme: 'funeral-costs'
+                        },
+                        summary: {
+                            title: 'Who else has contributed to the funeral costs?'
+                        }
+                    },
+                    required: ['q-applicant-funeral-costs-who-contributed'],
+                    propertyNames: {
+                        enum: ['q-applicant-funeral-costs-who-contributed']
+                    },
+                    allOf: [
+                        {
+                            properties: {
+                                'q-applicant-funeral-costs-who-contributed': {
+                                    type: 'string',
+                                    description:
+                                        'Enter the names of people, charities or organisations who helped to pay for the funeral.',
+                                    maxLength: 2000,
+                                    errorMessage: {
+                                        maxLength: 'Description must be 2000 characters or less'
+                                    }
+                                }
+                            }
+                        },
+                        {
+                            properties: {
+                                'funeral-costs': {
+                                    description:
+                                        '{% from "components/details/macro.njk" import govukDetails %}{{ govukDetails({summaryText: "What funeral costs you can claim?",html: \'<p class="govuk-body">Funeral costs can include:</p></p><ul class="govuk-list govuk-list--bullet"><li>funeral service (including casket, burial or cremation)</li><li>tombtone/memorial (if reasonable)</li><li>flowers</li><li>newspaper announcements</li><li>catering and refreshments (up to a limit of £500 only)</li><li>costs of transporting the person who died back to their country of origin</li></ul>\'}) }}'
                                 }
                             }
                         }
-                    }
-                },
-                {
-                    properties: {
-                        'funeral-costs': {
-                            description:
-                                '{% from "components/details/macro.njk" import govukDetails %}{{ govukDetails({summaryText: "What funeral costs you can claim?",html: \'<p class="govuk-body">Funeral costs can include:</p></p><ul class="govuk-list govuk-list--bullet"><li>funeral service (including casket, burial or cremation)</li><li>tombtone/memorial (if reasonable)</li><li>flowers</li><li>newspaper announcements</li><li>catering and refreshments (up to a limit of £500 only)</li><li>costs of transporting the person who died back to their country of origin</li></ul>\'}) }}'
+                    ],
+                    errorMessage: {
+                        required: {
+                            'q-applicant-funeral-costs-who-contributed':
+                                'Tell us who else has contributed to the funeral costs'
                         }
                     }
                 }
             ],
-            errorMessage: {
-                required: {
-                    'q-applicant-funeral-costs-who-contributed':
-                        'Tell us who else has contributed to the funeral costs'
-                }
-            },
             examples: [{}],
             invalidExamples: [
                 {
