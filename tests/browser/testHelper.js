4815be3c995c5d642fde648ede80d899b4efc68b
neil-stephen-mcgonigle <Neil.Mcgonigle@CICA.GOV.UK>
2023-03-16 15:33:01 +0000

    Refactor browser tests out to helper

--- tests/browser/testHelper.js
+++ tests/browser/testHelper.js
@@ -1,1 +1,47 @@
 'use strict';
+
+const jp = require('jsonpath');
+const {
+    click,
+    write,
+    // text,
+    into,
+    textBox
+} = require('taiko');
+
+function isDateQuestion(questionId, section) {
+    const format = jp.query(section, `$..properties["${questionId}"].format`);
+    if (format.length !== 0 && format[0] === 'date-time') {
+        return true;
+    }
+    return false;
+}
+exports.isDateQuestion = isDateQuestion;
+async function enterDateComponentsIntoTextBoxes(section, answer) {
+    const precision = jp.query(section, `$..precision`)[0];
+    // date is in ISO 8601 format
+    const d = new Date(answer);
+    if (precision.includes('DD')) {
+        await write(d.getDay(), into(textBox('Day')));
+    }
+    await write(d.getMonth(), into(textBox('Month')));
+    await write(d.getFullYear(), into(textBox('Year')));
+}
+exports.enterDateComponentsIntoTextBoxes = enterDateComponentsIntoTextBoxes;
+async function enterAnswerBrowserTests(questionnaire, pageId, questionId, answer) {
+    const section = questionnaire.sections[pageId];
+    if (isDateQuestion(questionId, section)) {
+        await enterDateComponentsIntoTextBoxes(section, answer);
+        return;
+    }
+    // $..properties["q--new-or-existing-application"].oneOf
+    const isSelectable = jp.query(section, `$..properties["${questionId}"].oneOf`).length === 1;
+
+    if (isSelectable) {
+        await click(answer);
+    } else {
+        const questionTitle = jp.query(section, `$..properties["${questionId}"].title`);
+        await write(answer, into(textBox(questionTitle[0])));
+    }
+}
+exports.enterAnswerBrowserTests = enterAnswerBrowserTests;
--- tests/step_implementation.js
+++ tests/step_implementation.js
@@ -20,11 +20,7 @@ const {
 } = require('taiko');
 const templates = require('./templateFactory');
 const {answerQuestion} = require('./routing/testHelper');
-const {
-    isDateQuestion,
-    enterDateComponentsIntoTextBoxes,
-    enterAnswerBrowserTests
-} = require('./isDateQuestion');
+const {enterAnswerBrowserTests} = require('./browser/testHelper');
 
 const applicationEntryPointUrl = process.env.application_entry_point_url;
 const windowSizeWidth = process.env.window_size_width;
@@ -111,20 +107,6 @@ step('Then the user is on page <pageId>', async function(pageId) {
 });
 
 /* eslint func-names: ["error", "never"] */
-step('When they answer <answer> to question <questionId>', async function(answer, questionId) {
-    if (runBrowserTests) {
-        const section = questionnaire.sections[currentBrowserTestPageId];
-        if (isDateQuestion(questionId, section)) {
-            await enterDateComponentsIntoTextBoxes(section, answer);
-        } else {
-            await click(answer);
-        }
-    } else {
-        answerQuestion(questionnaire, questionId, answer);
-    }
-});
-
-/* eslint func-names: ["error", "never"] */
 step('And they <buttonName>', async function(buttonName) {
     if (runBrowserTests) {
         await click(buttonName);
@@ -136,6 +118,15 @@ step('And they <buttonName>', async function(buttonName) {
         );
         const answeredQuestionnaire = nextSection.context;
         questionnaire = answeredQuestionnaire;
+    }
+});
+
+/* eslint func-names: ["error", "never"] */
+step('When they answer <answer> to question <questionId>', async function(answer, questionId) {
+    if (runBrowserTests) {
+        await enterAnswerBrowserTests(questionnaire, currentBrowserTestPageId, questionId, answer);
+    } else {
+        answerQuestion(questionnaire, questionId, answer);
     }
 });
 
