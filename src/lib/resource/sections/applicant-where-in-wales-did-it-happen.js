830a5ed6b83b45ff8235e01153e1318334429052
stephenjmcneill1971 <stephen.mcneill@cica.gov.uk>
2023-04-03 10:17:02 +0100

    Fixed routing to miss reporting crime page for deceased journey for incidents in Wales and England

--- src/lib/resource/sections/applicant-where-in-wales-did-it-happen.js
+++ src/lib/resource/sections/applicant-where-in-wales-did-it-happen.js
@@ -96,6 +96,10 @@ module.exports = {
         on: {
             ANSWER: [
                 {
+                    target: 'p--which-police-force-is-investigating-the-crime',
+                    cond: ['==', '$.answers.p-applicant-fatal-claim.q-applicant-fatal-claim', true]
+                },
+                {
                     target: 'p-applicant-describe-incident',
                     cond: [
                         '==',
