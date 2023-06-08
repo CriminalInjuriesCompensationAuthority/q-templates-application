1ab092f3fb12e3dcef7e74caf0268685186dad4f
Thomas Burton <Thomas.Burton@CICA.gov.uk>
2023-06-08 12:02:39 +0100

    Change victim to claimant on mainapplicant-parent

--- src/lib/resource/sections/mainapplicant-parent.js
+++ src/lib/resource/sections/mainapplicant-parent.js
@@ -10,9 +10,9 @@ module.exports = {
             properties: {
                 'q-mainapplicant-parent': {
                     type: 'boolean',
-                    title: "Are you the victim's parent?",
+                    title: "Are you the claimant's parent?",
                     description:
-                        'This means you have parental responsibility for the victim as their birth, step or adoptive parent.',
+                        'This means you have parental responsibility for the claimant as their birth, step or adoptive parent.',
                     oneOf: [
                         {
                             title: 'Yes',
@@ -30,12 +30,12 @@ module.exports = {
                     }
                 },
                 'can-i-apply-for-child': {
-                    description: `{% from "components/details/macro.njk" import govukDetails %}{{ govukDetails({summaryText: "Can I apply for the victim?",html: "<p class='govuk-body'>You need to prove you have parental responsibility for the victim to apply.</p><p class='govuk-body'>To prove this, you'll need relevant documents your name is on, such as:</p><ul class='govuk-list govuk-list--bullet'><li>the victim's full birth certificate</li><li>adoption documents</li><li>a parental responsibility agreement</li><li>a court order</li></ul><p class='govuk-body'>If you're still unsure about parental responsibility and what it means, you can find out more <a target='_blank' href='https://www.gov.uk/parental-rights-responsibilities/who-has-parental-responsibility'>on the UK Government website(opens in new tab)</a>.</p>"})}}`
+                    description: `{% from "components/details/macro.njk" import govukDetails %}{{ govukDetails({summaryText: "Can I apply for the claimant?",html: "<p class='govuk-body'>You need to prove you have parental responsibility for the claimant to apply.</p><p class='govuk-body'>To prove this, you'll need relevant documents your name is on, such as:</p><ul class='govuk-list govuk-list--bullet'><li>the claimant's full birth certificate</li><li>adoption documents</li><li>a parental responsibility agreement</li><li>a court order</li></ul><p class='govuk-body'>If you're still unsure about parental responsibility and what it means, you can find out more <a target='_blank' href='https://www.gov.uk/parental-rights-responsibilities/who-has-parental-responsibility'>on the UK Government website(opens in new tab)</a>.</p>"})}}`
                 }
             },
             errorMessage: {
                 required: {
-                    'q-mainapplicant-parent': "Select yes if you are the victim's parent"
+                    'q-mainapplicant-parent': "Select yes if you are the claimant's parent"
                 }
             },
             examples: [
