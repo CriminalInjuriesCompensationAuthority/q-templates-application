d36728cc81fe93d9d511e7e067c2a56c3f307fc0
stephenjmcneill1971 <stephen.mcneill@cica.gov.uk>
2023-06-09 13:52:25 +0100

    Added deceased journey other compensation changes
    Corected format of page mainapplicant-shared-responsibility-name so that it dislays correctly

--- src/lib/resource/sections/applicant-when-will-you-find-out.js
+++ src/lib/resource/sections/applicant-when-will-you-find-out.js
@@ -2,16 +2,44 @@
 
 module.exports = {
     section: {
+        l10n: {
+            vars: {
+                lng: 'en',
+                ns: 'p-when-will-you-find-out'
+            },
+            translations: [
+                {
+                    language: 'en',
+                    namespace: 'p-when-will-you-find-out',
+                    resources: {
+                        'when-will-you-find-out': {
+                            title: {
+                                myself:
+                                    'When will you find out if you’ve been awarded compensation?',
+                                proxy:
+                                    'When will the claimant find out if they’ve been awarded compensation or damages?'
+                            }
+                        }
+                    }
+                }
+            ]
+        },
         schema: {
             $schema: 'http://json-schema.org/draft-07/schema#',
             type: 'object',
             required: ['q-when-will-you-find-out'],
             properties: {
                 'q-when-will-you-find-out': {
-                    title: "When will you find out if you've been awarded compensation?",
+                    title: [
+                        '|l10nt',
+                        ['|role.all', 'myself'],
+                        'when-will-you-find-out.title.myself',
+                        ['|role.all', 'proxy'],
+                        'when-will-you-find-out.title.proxy'
+                    ],
                     type: 'string',
                     description:
-                        'Enter an approximate date, for example, December 2020. If you do not know you can say so.',
+                        'Enter an approximate date, for example, December 2023. If you do not know you can say so.',
                     maxLength: 50,
                     errorMessage: {
                         maxLength: 'When will you find out must be 50 characters or less'
@@ -21,7 +49,13 @@ module.exports = {
                             theme: 'other-compensation'
                         },
                         summary: {
-                            title: 'When will you find out their decision?'
+                            title: [
+                                '|l10nt',
+                                ['|role.all', 'myself'],
+                                'when-will-you-find-out.title.myself',
+                                ['|role.all', 'proxy'],
+                                'when-will-you-find-out.title.proxy'
+                            ]
                         }
                     }
                 }
