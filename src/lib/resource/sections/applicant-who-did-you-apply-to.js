0cc253185b3bace83e6943885a0790c72bf6fae0
stephenjmcneill1971 <stephen.mcneill@cica.gov.uk>
2023-06-09 13:52:25 +0100

    Added deceased journey other compensation changes
    Corected format of page mainapplicant-shared-responsibility-name so that it dislays correctly

--- src/lib/resource/sections/applicant-who-did-you-apply-to.js
+++ src/lib/resource/sections/applicant-who-did-you-apply-to.js
@@ -2,13 +2,46 @@
 
 module.exports = {
     section: {
+        l10n: {
+            vars: {
+                lng: 'en',
+                ns: 'p-applicant-who-did-you-apply-to'
+            },
+            translations: [
+                {
+                    language: 'en',
+                    namespace: 'p-applicant-who-did-you-apply-to',
+                    resources: {
+                        'applicant-who-did-you-apply-to': {
+                            title: {
+                                myself: 'Who have you applied to or received compensation from?',
+                                proxy:
+                                    'Who have they applied to or received compensation or damages from?'
+                            },
+                            error: {
+                                myself:
+                                    'Who you applied to or received compensation from must be 50 characters or less',
+                                proxy:
+                                    'Who have they applied to or received compensation or damages from must be 50 characters or less'
+                            }
+                        }
+                    }
+                }
+            ]
+        },
         schema: {
             $schema: 'http://json-schema.org/draft-07/schema#',
             type: 'object',
             required: ['q-applicant-who-did-you-apply-to'],
             properties: {
                 'q-applicant-who-did-you-apply-to': {
-                    title: 'Who have you applied to or received compensation from?',
+                    title: [
+                        '|l10nt',
+                        ['|role.all', 'myself'],
+                        'applicant-who-did-you-apply-to.title.myself',
+                        ['|role.all', 'proxy'],
+                        'applicant-who-did-you-apply-to.title.proxy'
+                    ],
                     type: 'string',
                     maxLength: 50,
                     errorMessage: {
@@ -20,15 +53,26 @@ module.exports = {
                             theme: 'other-compensation'
                         },
                         summary: {
-                            title: 'Who have you applied for compensation from?'
+                            title: [
+                                '|l10nt',
+                                ['|role.all', 'myself'],
+                                'applicant-who-did-you-apply-to.title.myself',
+                                ['|role.all', 'proxy'],
+                                'applicant-who-did-you-apply-to.title.proxy'
+                            ]
                         }
                     }
                 }
             },
             errorMessage: {
                 required: {
-                    'q-applicant-who-did-you-apply-to':
-                        'Enter who you applied to or received compensation from'
+                    'q-applicant-who-did-you-apply-to': [
+                        '|l10nt',
+                        ['|role.all', 'myself'],
+                        'applicant-who-did-you-apply-to.error.myself',
+                        ['|role.all', 'proxy'],
+                        'applicant-who-did-you-apply-to.error.proxy'
+                    ]
                 }
             },
             examples: [
