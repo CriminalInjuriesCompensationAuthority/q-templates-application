92c8f09a6fd6aac874f95a699766022ee56f5884
stephenjmcneill1971 <stephen.mcneill@cica.gov.uk>
2023-07-24 16:56:06 +0100

    Changed content for applicant-fatal-claim

--- src/lib/resource/sections/applicant-fatal-claim.js
+++ src/lib/resource/sections/applicant-fatal-claim.js
@@ -13,13 +13,15 @@ module.exports = {
                     title: 'Select why you are applying',
                     oneOf: [
                         {
-                            title: 'I am applying because of an injury',
+                            title: 'I am applying because I was the victim of a violent crime',
                             description:
-                                'This could include abuse over a period of time, or a disabling mental injury as a result of witnessing a crime.',
+                                'This could include a physical injury, disabling mental injury or abuse over a period of time. The disabling mental injury could be due to witnessing a loved one being injured in a violent crime.',
                             const: false
                         },
                         {
-                            title: 'I am applying because someone has died',
+                            title: 'I am applying because someone died due to a violent crime',
+                            description:
+                                'If you are paying for the funeral of the person who died, you may be able to get help with the cost. If you are their relative, you may also be eligible for bereavement payments.',
                             const: true
                         }
                     ],
@@ -32,13 +34,17 @@ module.exports = {
                         },
                         integration: {
                             hideOnSummary: true
-                        }            
+                        }
                     }
+                },
+                'applicant-fatal-claim-info': {
+                    description:
+                        '{% from "components/details/macro.njk" import govukDetails %}{{ govukDetails({summaryText: "If both options apply", html:\'<p class="govuk-body">If both of the options above apply you’ll need to make 2 separate applications.</p><p class="govuk-body">You can make the applications in any order but you’ll need to select a different option on this page each time.</p>\'}) }}'
                 }
             },
             errorMessage: {
                 required: {
-                    'q-applicant-fatal-claim': 'Select why you are applying'
+                    'q-applicant-fatal-claim': 'Select the reason you’re applying'
                 }
             },
             examples: [
