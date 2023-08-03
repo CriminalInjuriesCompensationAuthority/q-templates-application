3d63bf84a314312c8b6a2e54deb7339fc4323acb
Jennifer Brown <jennifer.brown@informed.com>
2023-08-03 17:46:27 +0100

    fix syntax

--- src/lib/resource/sections/confirmation.js
+++ src/lib/resource/sections/confirmation.js
@@ -26,7 +26,7 @@ module.exports = {
                                     {% endif %}
                                     {% if secondaryReference and caseReference %}}
                                     {% set html =  "<p>The reference number for your bereavement claim is: <br /><strong>" + caseReferenceNumber + "</strong></p><p><strong>The reference number for your funeral costs claim is:" + secondaryReference + </strong></p><p>We have sent a confirmation to <strong>" + contactMethod + "</strong></p>" %}
-                                    {% elif caseReferenceNumber %}
+                                    {% else if caseReferenceNumber %}
                                       {% set html =  "<p>Your reference number is <br /><strong>" + caseReferenceNumber + "</strong></p><p>We have sent a confirmation to <strong>" + contactMethod + "</strong></p>" %}
                                     {% else %}
                                       {% set html =  "<p>We\'ll send your confirmation to <strong>" + contactMethod + "</strong> soon</p>" %}
