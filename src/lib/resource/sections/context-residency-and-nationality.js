76e2228704ef28b0a9df4f1522634cbbbeadb4db
neil-stephen-mcgonigle <Neil.Mcgonigle@CICA.GOV.UK>
2023-08-14 17:26:57 +0100

    fix residency context page contextualisation

--- src/lib/resource/sections/context-residency-and-nationality.js
+++ src/lib/resource/sections/context-residency-and-nationality.js
@@ -15,13 +15,20 @@ module.exports = {
                         'residency-context': {
                             title: {
                                 applicant: 'About your residency and nationality',
-                                proxy: 'About the claimant’s residency and nationality'
+                                proxy: {
+                                    nonDeceased: 'About the victim’s residency and nationality',
+                                    deceased: 'About the claimant’s residency and nationality'
+                                }
                             },
                             description: {
                                 applicant:
                                     '<p class="govuk-body">We\'re going to ask you some questions about your residency and nationality when the crime happened. This is so we can work out if you\'re eligible to get compensation from us for this crime.</p><p class="govuk-body"><a href="https://www.gov.uk/guidance/criminal-injuries-compensation-residency-and-nationality" target="_blank">Find out how the Criminal Injuries Compensation Scheme\'s residency and nationality requirements work</a>.</p>',
-                                proxy:
-                                    '<p class="govuk-body">We\'re going to ask you some questions about the  claimant\'s residency and nationality when the crime happened. This is so we can work out if they\'re eligible to get compensation from us for this crime.</p><p class="govuk-body"><a href="https://www.gov.uk/guidance/criminal-injuries-compensation-residency-and-nationality" target="_blank">Find out how the Criminal Injuries Compensation Scheme\'s residency and nationality requirements work</a>.</p>'
+                                proxy: {
+                                    nonDeceased:
+                                        '<p class="govuk-body">We\'re going to ask you some questions about the victim\'s residency and nationality when the crime happened. This is so we can work out if they\'re eligible to get compensation from us for this crime.</p><p class="govuk-body"><a href="https://www.gov.uk/guidance/criminal-injuries-compensation-residency-and-nationality" target="_blank">Find out how the Criminal Injuries Compensation Scheme\'s residency and nationality requirements work</a>.</p>',
+                                    deceased:
+                                        '<p class="govuk-body">We\'re going to ask you some questions about the claimant\'s residency and nationality when the crime happened. This is so we can work out if they\'re eligible to get compensation from us for this crime.</p><p class="govuk-body"><a href="https://www.gov.uk/guidance/criminal-injuries-compensation-residency-and-nationality" target="_blank">Find out how the Criminal Injuries Compensation Scheme\'s residency and nationality requirements work</a>.</p>'
+                                }
                             }
                         }
                     }
@@ -37,15 +44,19 @@ module.exports = {
                     //prettier-ignore
                     title: [
                         '|l10nt',
-                        ['|role.all', 'proxy'],
-                        'residency-context.title.proxy',
+                        ['|role.all', 'proxy', 'nonDeceased'],
+                        'residency-context.title.proxy.nonDeceased',
+                        ['|role.all', 'proxy', 'deceased'],
+                        'residency-context.title.proxy.deceased',
                         ['|role.all'],
                         'residency-context.title.applicant'
                     ],
                     description: [
                         '|l10nt',
-                        ['|role.all', 'proxy'],
-                        'residency-context.description.proxy',
+                        ['|role.all', 'proxy', 'nonDeceased'],
+                        'residency-context.description.proxy.nonDeceased',
+                        ['|role.all', 'proxy', 'deceased'],
+                        'residency-context.description.proxy.deceased',
                         ['|role.all'],
                         'residency-context.description.applicant'
                     ]
