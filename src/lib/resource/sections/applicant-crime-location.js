6cf1c1d13fcba63cd418abed6d6c8c35dd1e9ec3
stephenjmcneill1971 <stephen.mcneill@cica.gov.uk>
2023-08-18 11:20:22 +0100

    Added crime location page

---
+++ src/lib/resource/sections/applicant-crime-location.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = {
+    section: {
+        schema: {
+            $schema: 'http://json-schema.org/draft-07/schema#',
+            type: 'object',
+            required: ['q-applicant-crime-location'],
+            properties: {
+                'q-applicant-crime-location': {
+                    type: 'string',
+                    title: 'Tell us more about where the crime happened',
+                    maxLength: 140,
+                    errorMessage: {
+                        maxLength: 'Location must be 140 characters or less'
+                    },
+                    meta: {
+                        classifications: {
+                            theme: 'crime'
+                        },
+                        summary: {
+                            title: 'Tell us more about where the crime happened'
+                        }
+                    }
+                }
+            },
+            errorMessage: {
+                required: {
+                    'q-applicant-crime-location': 'Enter the location where the crime happened'
+                }
+            },
+            examples: [
+                {
+                    'q-applicant-crime-location': 'Some description'
+                }
+            ],
+            invalidExamples: [
+                {
+                    'q-applicant-crime-location': null
+                }
+            ]
+        }
+    },
+    route: {
+        on: {
+            ANSWER: [
+                {
+                    target: 'p-applicant-describe-incident',
+                    cond: [
+                        '==',
+                        '$.answers.p--was-the-crime-reported-to-police.q--was-the-crime-reported-to-police',
+                        false
+                    ]
+                },
+                {
+                    target: 'p--when-was-the-crime-reported-to-police',
+                    cond: [
+                        '==',
+                        '$.answers.p--was-the-crime-reported-to-police.q--was-the-crime-reported-to-police',
+                        true
+                    ]
+                }
+            ]
+        }
+    }
+};
