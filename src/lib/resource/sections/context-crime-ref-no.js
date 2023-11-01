7a6f3e9439348b238d8c7a71d424e36d2e742577
stephenjmcneill1971 <stephen.mcneill@cica.gov.uk>
2023-11-01 12:20:11 +0000

    Corrected grammar missing apostrophe in text of context-crime-ref-no page

--- src/lib/resource/sections/context-crime-ref-no.js
+++ src/lib/resource/sections/context-crime-ref-no.js
@@ -11,7 +11,7 @@ module.exports = {
                     title:
                         "You'll need to give us the crime reference number as part of this application",
                     description:
-                        '<p class="govuk-body">The police give every crime a reference number when it\'s reported. This is called a crime reference number. You\'ll need to give us this crime reference number as part of this application so you should make sure you know it.</p><p class="govuk-body">If you do not know your crime reference number, call 101 to speak to your local police station. They\'ll be able to tell you the crime reference number. You\'ll need to give them some details about the crime to help them find the correct case.</p><p class="govuk-body">If you do not have the crime reference number to hand, you can continue with your application but youll need to enter it later.</p>'
+                        '<p class="govuk-body">The police give every crime a reference number when it\'s reported. This is called a crime reference number. You\'ll need to give us this crime reference number as part of this application so you should make sure you know it.</p><p class="govuk-body">If you do not know your crime reference number, call 101 to speak to your local police station. They\'ll be able to tell you the crime reference number. You\'ll need to give them some details about the crime to help them find the correct case.</p><p class="govuk-body">If you do not have the crime reference number to hand, you can continue with your application but you\'ll need to enter it later.</p>'
                 }
             },
             examples: [{}],
