9fdb197d3ec62a01e81e6c2885d079f6119627d7
Barry Piccinni <barry.piccinni@CICA.GOV.UK>
2024-01-22 15:45:21 +0000

    Update applicant-you-cannot-get-compensation

--- src/lib/resource/sections/applicant-you-cannot-get-compensation.js
+++ src/lib/resource/sections/applicant-you-cannot-get-compensation.js
@@ -8,9 +8,9 @@ module.exports = {
             additionalProperties: false,
             properties: {
                 'you-cannot-get-compensation': {
-                    title: 'You cannot get compensation',
+                    title: "You cannot get compensation as you've told us the crime has not been reported to the police",
                     description:
-                        '{% from "components/warning-text/macro.njk" import govukWarningText %}<p class="govuk-body">You cannot get compensation if the crime has not been reported to the police.</p>{{ govukWarningText({text: "If you submit a claim before the crime has been reported to the police, the claim will be refused and you will not be able to try again.",iconFallbackText: "Warning"}) }}'
+                        '<p class="govuk-body">You can only get compensation from us if the crime has been reported to the police. This must have been done as soon as reasonably possible.</p>'
                 }
             },
             examples: [{}],
