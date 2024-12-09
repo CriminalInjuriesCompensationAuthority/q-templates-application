bd27c55139583470f79fe3d4ba58e99cd179976f
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2024-12-09 15:30:54 +0000

    Update CRN context page content

--- src/lib/resource/sections/context-crime-ref-no.js
+++ src/lib/resource/sections/context-crime-ref-no.js
@@ -8,9 +8,9 @@ module.exports = {
             additionalProperties: false,
             properties: {
                 'p--context-crime-ref-no': {
-                    title: "You'll need to tell us your crime reference number",
+                    title: 'About the crime reference number',
                     description:
-                        '<p class="govuk-body">The police give every crime a reference number when it\'s reported.</p> <p class="govuk-body">You\'ll need to give us this crime reference number as part of this application, so you should make sure you know it.</p><h2 class="govuk-heading-l">If you don\'t know your crime reference number</h2><p class="govuk-body">To get your crime reference number, call 101 to speak to your local police station. You\'ll need to give them some details about the crime to help them find the correct case.</p><h2 class="govuk-heading-l">Continuing with your application</h2><p class="govuk-body">If you do not have the crime reference number to hand, you can continue with your application, but you\'ll need to enter it later.</p>'
+                        '<p class="govuk-body">The police give every crime a reference number when it\'s reported. This is called a crime reference number. You\'ll need to give us this as part of this application.</p><h2 class="govuk-heading-l">If you do not know your crime reference number</h2><p class="govuk-body">To get your crime reference number, call 101 to speak to your local police. You\'ll have to give them some details about the crime to help them find your case.</p><h2 class="govuk-heading-l">Continuing with your application</h2><p class="govuk-body">You\'ll need to enter the crime reference number later, before you can submit the application.</p>'
                 }
             },
             meta: {
