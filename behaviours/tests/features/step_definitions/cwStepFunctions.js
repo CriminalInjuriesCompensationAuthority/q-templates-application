3d7fd04165b415d6499c70cd4d59ac9b07be5e5c
tjbburton <Thomas.Burton@CICA.gov.uk>
2025-06-05 11:25:29 +0100

    Add cw tasklist bdd step definitions

--- behaviours/tests/features/step_definitions/cwStepFunctions.js
+++ behaviours/tests/features/step_definitions/cwStepFunctions.js
@@ -241,11 +241,14 @@ async function clickSigninLink(testObject) {
 }
 
 async function checkTaskStatus(testObject, task, status) {
-    return 'ok';
+    const statusOnPage = await testObject.page.locator(`.${task}`).textContent();
+    assert.ok(statusOnPage.trim() === status);
 }
 
-async function selectTask(testObject, task, status) {
-    return 'ok';
+async function selectTask(testObject, task) {
+    await testObject.page
+        .locator('.govuk-task-list__item', {has: testObject.page.locator(`.${task}`)})
+        .click();
 }
 
 module.exports = {
